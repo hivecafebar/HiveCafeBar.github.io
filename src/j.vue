7263e67f2e6e549636bdfe588708b7c89aad739e
Kadae <kadae@yandex.ru>
2023-11-14 19:55:03 +0500

    Check drinks.

--- src/j.vue
+++ src/j.vue
@@ -1,22 +1,8 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Footer from './components/Footer.vue'
-import Navigation from './components/Navigation.vue'
 </script>
 
 <template>
   <main id="﹟main" style="background-color:rgb(10, 10, 10)">
-    <progress class="﹟progress" value="0" max="100"></progress>
-    <audio id="ID_AC_sound" preload="auto"><source src="./assets/media/AchievementUnlocked.mp3" type="audio/mpeg" /></audio>
-    <audio id="ID_CC_sound" preload="auto"><source src="./assets/media/AchievementUnlocked.mp3" type="audio/mpeg" /></audio>
-
-    <Navigation />
-    <div id="﹟content" class="♹ ♹♺ ◬">
-      <article id="﹟article" style="background-color:black;">
-        <RouterView />
-        <Footer msg="Out ambition is unlimited." />
-      </article>
-    </div>
   </main>
 </template>
 
